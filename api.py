8e21da7509023f70fcee6ebd7890e9b212dedfee
RodResq <rodrigoresque@yahoo.com.br>
2025-02-25 17:44:31 -0300

    updt

--- api.py
+++ api.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html', titulo='Jogos')
+    jogos = ['Tetris', 'Skyrim', 'Crash']
+    return render_template('lista.html', titulo='Jogos', lista=jogos)
 
 if __name__ == '__main__':
     app.run(debug=True)
