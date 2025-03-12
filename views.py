aa7f7f3f14ead6fc11f79d67c345e1fc2d152fdb
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-03-12 14:46:48 -0300

    quarta 1st

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, session, flash, url_for
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from models import Jogos, Usuarios
 from api import app, db
 
@@ -95,3 +95,8 @@ def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect(url_for('index'))
+
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
