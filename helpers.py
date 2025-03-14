2a7701bb7e87a6c885f2b3d860970d3d8d7a0e49
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-03-14 08:50:14 -0300

    sexta 1st

--- helpers.py
+++ helpers.py
@@ -1,3 +1,14 @@
 import os
-
 from api import app
+
+def recupera_imagem(id):
+    for nome_arquivo in os.listdir(app.config['PATH_UPLOADS']):
+        if f'capa{id}' in nome_arquivo:
+            return nome_arquivo
+    return 'capa_padrao.jpg'
+        
+        
+def deleta_arquivo(id):
+    arquivo = recupera_imagem(id)
+    if arquivo != 'capa_padrao.jpg':
+        os.remove(os.path.join(app.config['PATH_UPLOADS'], arquivo))
